b60edaa7cb3f695db872b414fd20751e815f0de7
VladymyrB <67546066+VladymyrB@users.noreply.github.com>
2020-09-16 18:06:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напруга: ");
+            double Voltage = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Сила току: ");
+            double CurrentStrength = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Сопротевление:" + Voltage/CurrentStrength);
+            Console.ReadLine();
+        }
+    }
+}
