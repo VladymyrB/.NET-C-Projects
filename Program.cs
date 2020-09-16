c62c4bcb6e03bbcad31c69699fed0eb6514744d9
VladymyrB <67546066+VladymyrB@users.noreply.github.com>
2020-09-16 18:09:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sum = 0.0;
+            double Product = 1;
+            int NN = 0;
+            int NM = 0;
+            do
+            {
+                Console.WriteLine("NN: ");
+                NN = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("NM: ");
+                NM = Int32.Parse(Console.ReadLine());
+            } while (NN < 0 && NM < 0);
+
+            
+           
+
+                for (int i = NN; i <= NM; i++)
+                {
+                    Sum += ((i ^ 2) - ((-1) ^ (i + 1)) * i ^ 3) / ((i ^ 2) + i + 1);
+                }
+                for (int i = NN; i <= NM; i++)
+                {
+                    Product *= ((i ^ 2) - ((-1) ^ (i + 1)) * i ^ 3) / ((i ^ 2) + i + 1);
+                }
+
+                Console.WriteLine($"Сума ряда: {Sum}  ");
+                Console.WriteLine($"Произведение ряда: {Product}  ");
+                Console.ReadLine();
+
+            
+         
+
+        }
+    }
+}
