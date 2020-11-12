0b6aeed7539574c8f974f2b077169ac36610497a
Volodymyr Bondarenko <67546066+VladymyrB@users.noreply.github.com>
2020-11-12 22:55:14 +0200

    Add files via upload

---
+++ ButtonPhone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8_2
+{
+    class ButtonPhone: DiscPhone
+    {
+        public override List<char> CharSet { get; protected set; } = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9','*','&' };
+
+        public override void GetCall(DiscPhone phone )
+        {
+            Console.WriteLine($"Calling number: {phone.PhoneNumber}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SmartPhone();
+            var phone2 = new ButtonPhone() { PhoneNumber = "2342342" };
+            phone.GetCall(phone2);
+            Console.ReadLine();
+        }
+    }
+}
