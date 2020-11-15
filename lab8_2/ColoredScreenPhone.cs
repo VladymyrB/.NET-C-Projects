9902a2b8b87a0d072bc393f74662418e68eaa198
Volodymyr Bondarenko <67546066+VladymyrB@users.noreply.github.com>
2020-11-15 23:29:09 +0200

    Add files via upload

---
+++ lab8_2/ColoredScreenPhone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Lab8_2
+{
+    class ColoredScreenPhone: NotColoredScreenPhone
+    {
+        public List<string> ScreenColors { get; } = new List<string> { };
+        public List<string> SimCards { get; } = new List<string> { "First", "Second" };
+        public override void MakeCall()
+        {
+            Console.WriteLine("Choose Sim-Card: ");
+            if(Console.ReadLine() == "1" || Console.ReadLine() == "First")
+                Console.WriteLine("Call was made by first Sim-Card");
+            else if(Console.ReadLine() == "2" || Console.ReadLine() == "Second")
+                Console.WriteLine("Call was made by second Sim-Card");
+            else
+                Console.WriteLine("Sim-Card wasn't found");
+
+        }
+        public override void SendMsg()
+        {
+            Console.WriteLine("Choose Sim-Card: ");
+            if (Console.ReadLine() == "1" || Console.ReadLine() == "First")
+                Console.WriteLine("Msg was sent by first Sim-Card");
+            else if (Console.ReadLine() == "2" || Console.ReadLine() == "Second")
+                Console.WriteLine("Msg was sent by second Sim-Card");
+            else
+                Console.WriteLine("Sim-Card wasn't found");
+        }
+        public void MakeMMS()
+        {
+            Console.WriteLine("Choose Sim-Card: ");
+            if (Console.ReadLine() == "1" || Console.ReadLine() == "First")
+                Console.WriteLine("MMS was sent by first Sim-Card");
+            else if (Console.ReadLine() == "2" || Console.ReadLine() == "Second")
+                Console.WriteLine("MMS was sent by second Sim-Card");
+            else
+                Console.WriteLine("Sim-Card wasn't found");
+        }
+        public void GetMMS()
+        {
+            Console.WriteLine("MMS was gotten");
+        }
+    }
+}
---
+++ lab8_2/NotColoredScreenPhone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace Lab8_2
+{
+    class NotColoredScreenPhone: ButtonPhone
+    {
+        public override List<char> CharSet { get; protected set; } = new List<char> { };
+        public string Color { get; set; }
+        public string Resolution { get; set; }
+        public double Size { get; protected set; }
+        public virtual void SendMsg()
+        {
+            Console.WriteLine("Massage was sent");
+        }
+        public void GetMsg()
+        {
+            Console.WriteLine("Massage was gotten");
+        }
+    }
+}
